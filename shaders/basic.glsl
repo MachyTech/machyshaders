3844e2b77f9f56daed4641797cc3608d82284428
Timo33Thans <timotopper@gmail.com>
2022-08-15 09:59:15 -0400

    updated latest shaders

--- shaders/basic.glsl
+++ shaders/basic.glsl
@@ -15,6 +15,6 @@ mat2 rotate2d( float _angle){
 
 void machygraph_main(out vec4 color, in vec3 c, in vec2 t){
     //t.x *= u_tex0resolution.y/u_tex0resolution.x;
-    //color = vec4(t.x, t.y, 0.0, 1.0);
-    color = texture(u_tex0, t);
+    color = vec4(1.0, 1.0, 1.0, 1.0);
+    //color = texture(u_tex0, t);
 }
